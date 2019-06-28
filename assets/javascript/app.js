080cedd7c22457662aed8887bdb78bb26598e467
evepsmith15 <50637882+evepsmith15@users.noreply.github.com>
2019-06-27 19:20:02 -0700

    Add files via upload

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,63 @@
-//code for the timer
-var countdown = new Date("60").getTime();
-var t = setInterval(function() {
-  var now = new Date().getTime();
-  var distance = countdown - now;
-var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  document.getElementById("timer").innerHTML = seconds + "s ";
-  if (distance < 0) {
-    clearInterval(t);
-    document.getElementById("timer").innerHTML = "TIMES UP";
+var ending = $("results");
+
+//code for the trivia
+var trivia = [{
+    questions: "What is the meme of the month?",
+    answers: ['CavemanSponge', 'Were Irritating', 'CJ', 'Waaa'],
+    correct: 'CJ'
+  },
+  {
+    questions: "What is the most reconized twitch emote expression on Twitch?",
+    answers: ['Kappa', 'FeelsBadMan', 'BibleThump', 'Jebaited'],
+    correct: 'Kappa'
+  },
+  {
+    questions: "Which franchise has the most memes?",
+    answers: ['The Office', 'Spongebob', 'Pokemon', 'Star Trek'],
+    correct: 'Spongebob'
   }
-}, 60000);
+]
 
-//code for the quiz button 
-const ResultsContainer = document.getElementById('Results');
-const SubmitButton = document.getElementById('Submit');
-//code for answering the questions 
+var timer;
+var game = {
+  correct: 0,
+  wrong: 0,
+  counter: 120,
+  
+  //code for the timer
+  countdown: function () {
+    game.counter--;
+    $("#counter").html(game.counter);
+    if (game.counter === 0) {
+      console.log("TIME UP");
+      game.total();
+    }
+  },
 
-SubmitButton.addEventListener('click', Results);
-var Correct = 0;
-//correct answers: CJ, Kappa, Spongebob 
-//if (Answers === 
+  //starts the timer
+  start: function () {
+    timer = setInterval(game.countdown, 1000);
+    $("#timer").prepend(
+      "<h3>Time Remaining: <span id='counter'>120</span> Seconds</h3>"
+    );
+  },
+
+  //calculates the 3 scores
+  total: function () {
+    var check = ending.children("check:checked"); //checks the answer 
+    for (i = 0; i > check.length; i++) {
+      if ($(check[i]).val() === trivia[i].correct) {
+        game.correct++;
+      } else {
+        game.wrong++;
+      }
+    }
+    this.results(); //goes to the results screen 
+  },
+
+  //shows the answers 
+  results: function () {
+    ending.append("Questions correct: " + this.correct);
+    ending.append("Questions wrong: " + this.wrong);
+  }
+};
